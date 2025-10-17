2ca5deb3a98f5cadf02ffb6c80ec3263a7ae10be
0x471 <0x471@protonmail.com>
2025-10-17 01:46:00 +0100

    initial commit

---
+++ dsc-parser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dsc-parser
+//
+//  Created by Peter Pan on 17/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dsc-parser/dsc_parserApp.swift
@@ -0,0 +1,17 @@
+//
+//  dsc_parserApp.swift
+//  dsc-parser
+//
+//  Created by Peter Pan on 17/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct dsc_parserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
